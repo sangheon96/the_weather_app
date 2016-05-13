3f7ddd2d0947e4a4d0f7834197cce22309ea6fe0
sangheon96 <sangheon96@gmail.com>
2016-05-13 06:07:56 -0400

    Add files via upload

---
+++ Whats The Weather/Whats The Weather/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  Whats The Weather
+//
+//  Created by Stan Lee on 2016. 4. 14..
+//  Copyright © 2016년 Stan Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cityTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    @IBAction func findWeather(sender: AnyObject) {
+        
+        var wasSuccessful = false
+        
+        let attemptedUrl = NSURL(string: "http://www.weather-forecast.com/locations/" + cityTextField.text!.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
+        
+        if let url = attemptedUrl {
+        
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url) {(data, respone, error) -> Void in
+            
+            if let urlContent = data {
+                
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                
+                let websiteArray = webContent!.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
+                if websiteArray.count > 1 {
+                    
+                    let weatherArray = websiteArray[1].componentsSeparatedByString("</span>")
+                    
+                    if weatherArray.count > 1 {
+                        
+                        wasSuccessful = true
+                        
+                        let weatherSummary = weatherArray[0].stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                        
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            
+                            
+                            self.resultLabel.text = weatherSummary
+                        })
+                        
+                    }
+                    
+                    
+                }
+            }
+                
+                if wasSuccessful == false {
+                    self.resultLabel.text = "Couldn't find the weather for that city- Try again"
+                }
+            
+            
+            }
+        task.resume()
+        
+        } else {
+            
+            self.resultLabel.text = "Couldn't find the weather for that city- Try again"
+        }
+
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+            }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
